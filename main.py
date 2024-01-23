079824cc1c9ce91310c1d6ca6767d792b1da942b
Sayan__SD <65155412+sayandutta99@users.noreply.github.com>
2024-01-23 13:09:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 83348 06957', 'Hi Abhishek', 18, 37)
+pywhatkit.sendwhatmsg('+91 62914 38330', 'Hi bich automation ta complete complete', 16, 36)
+#pywhatkit.sendwhatmsg('+91 89108 18490', 'Hi dada automation ta complete ami ata automatic use', 16, 37)
